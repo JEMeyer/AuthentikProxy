2a8a2b9e1fc4f4eb9458c42c9f126749ae479f9c
Joe Meyer <joseph@meyer.id>
2024-07-08 13:03:41 -0500

    fix callback not working

--- Controllers/AuthentikController.cs
+++ Controllers/AuthentikController.cs
@@ -3,9 +3,9 @@ using Microsoft.Extensions.Options;
 using AuthentikProxy.Models;
 using System.Net.Http.Headers;
 using System.Text;
-using Microsoft.AspNetCore.Http.Extensions;
-using System.Net;
 using System.Web;
+using System.Text.Json;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace AuthentikProxy.Controllers
 {
@@ -20,9 +20,17 @@ namespace AuthentikProxy.Controllers
         {
             try
             {
-                request.TryGetValue("redirect_uri", out string? originalRedirectUri);
-                // request["redirect_uri"] = $"{Request.Scheme}://{Request.Host}{Request.PathBase}";
-                var httpClient = httpClientFactory.CreateClient();
+                request.TryGetValue("redirect_uri", out string? redirect_uri);
+                // Create state parameter with client_id for lookup in /callback
+                var state = new
+                {
+                    redirect_uri,
+                };
+                var stateJson = JsonSerializer.Serialize(state);
+                var stateEncoded = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(stateJson));
+
+                request["state"] = stateEncoded;
+                request["redirect_uri"] = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/Authentik/callback";
 
                 var queryString = string.Join("&", request.Select(kvp => $"{HttpUtility.UrlEncode(kvp.Key)}={HttpUtility.UrlEncode(kvp.Value)}"));
                 var authUrl = $"{_authentikSettings.Url}/application/o/authorize/?{queryString}";
@@ -51,7 +59,7 @@ namespace AuthentikProxy.Controllers
                 }
 
                 request["client_secret"] = client.ClientSecret;
-                request["redirect_uri"] = $"{Request.Scheme}://{Request.Host}{Request.PathBase}";
+                request["redirect_uri"] = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/Authentik/callback";
 
                 using var content = new FormUrlEncodedContent(request);
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
@@ -95,6 +103,39 @@ namespace AuthentikProxy.Controllers
 
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 return Content(jsonResponse, "application/json");
+            }
+            catch (HttpRequestException ex)
+            {
+                logger.LogError("HttpRequestException: {msg}", ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+
+        [HttpGet("callback")]
+        public IActionResult Callback([FromQuery] Dictionary<string, string> request)
+        {
+            try
+            {
+                request.TryGetValue("state", out string? stateEncoded);
+
+                if (stateEncoded != null)
+                {
+                    var stateJson = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(stateEncoded));
+                    var state = JsonSerializer.Deserialize<Dictionary<string, string>>(stateJson);
+
+                    if (state != null && state.TryGetValue("redirect_uri", out string? redirect_uri))
+                    {
+                        var httpClient = httpClientFactory.CreateClient();
+
+                        var queryString = string.Join("&", request.Select(kvp => $"{HttpUtility.UrlEncode(kvp.Key)}={HttpUtility.UrlEncode(kvp.Value)}"));
+                        var callbackUrl = $"{redirect_uri}?{queryString}";
+
+                        return Redirect(callbackUrl);
+                    }
+                }
+
+                return BadRequest("Invalid state parameter.");
             }
             catch (HttpRequestException ex)
             {
